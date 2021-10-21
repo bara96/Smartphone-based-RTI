ebedf0dfe6ae03a10e819d0a6d5f30e1293a7570
Matteo Baratella <matteo.baratella96@mail.com>
2021-10-21 15:34:04 +0200

    Updated calibration

--- camera_calibrator.py
+++ camera_calibrator.py
@@ -1,11 +1,14 @@
 # Import required modules
 import cv2
 import numpy as np
-import os
+import os, random
 import shutil
 import glob
 
 
+# generate the frames images from the video, return the n° of frames generated
+# path: video path
+# from_scratch: if true will regenerate the entire frame sequence from scratch
 def generate_video_frames(path, from_scratch=False):
     frames_dir = "assets/frames"
 
@@ -46,9 +49,15 @@ def generate_video_frames(path, from_scratch=False):
 
     cap.release()
     cv2.destroyAllWindows()
+    return i
 
 
-def calibrate_camera():
+# Calibrate the camera reading frames from generated assets
+# n_frames: n° of random frames to use for the calibration
+# show_images: if True, display the calibrated images
+def calibrate_camera(n_frames=20, show_images=True):
+    frames_dir = 'assets/frames/'
+
     # Define the dimensions of checkerboard
     CHECKERBOARD = (6, 9)
 
@@ -63,7 +72,7 @@ def calibrate_camera():
     # Vector for 2D points
     twodpoints = []
 
-    grayColor = None
+    gray_color = None
 
     #  3D points real world coordinates
     objectp3d = np.zeros((1, CHECKERBOARD[0]
@@ -73,21 +82,19 @@ def calibrate_camera():
                           0:CHECKERBOARD[1]].T.reshape(-1, 2)
     prev_img_shape = None
 
-    # Extracting path of individual image stored
-    # in a given directory. Since no path is
-    # specified, it will take current directory
-    # jpg files alone
-    images = glob.glob('%s/frame*.png' % 'assets/frames')
-
-    for filename in images:
-        image = cv2.imread(filename)
-        grayColor = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Extracting path of random images stored in a given directory.
+    # images = glob.glob('%s/frame*.png' % frames_dir)
+    for i in range(n_frames):
+        filename = random.choice(os.listdir(frames_dir))
+        print("Selected: ", frames_dir + filename)
+        image = cv2.imread(frames_dir + filename)
+        gray_color = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         # Find the chess board corners
         # If desired number of corners are
         # found in the image then ret = true
         ret, corners = cv2.findChessboardCorners(
-            grayColor, CHECKERBOARD,
+            gray_color, CHECKERBOARD,
             cv2.CALIB_CB_ADAPTIVE_THRESH
             + cv2.CALIB_CB_FAST_CHECK +
             cv2.CALIB_CB_NORMALIZE_IMAGE)
@@ -101,18 +108,18 @@ def calibrate_camera():
             # Refining pixel coordinates
             # for given 2d points.
             corners2 = cv2.cornerSubPix(
-                grayColor, corners, (11, 11), (-1, -1), criteria)
+                gray_color, corners, (11, 11), (-1, -1), criteria)
 
             twodpoints.append(corners2)
 
             # Draw and display the corners
             image = cv2.drawChessboardCorners(image, CHECKERBOARD, corners2, ret)
 
-        # cv2.imshow('img', image)
-        # cv2.waitKey(0)
+        if show_images:
+            cv2.imshow('img', image)
+            cv2.waitKey(0)
 
     cv2.destroyAllWindows()
-
 
     # Perform camera calibration by
     # passing the value of above found out 3D points (threedpoints)
@@ -120,7 +127,7 @@ def calibrate_camera():
     # detected corners (twodpoints)
     print("\n\n")
     print("Calibrating...")
-    (ret, matrix, distortion, r_vecs, t_vecs) = cv2.calibrateCamera(threedpoints, twodpoints, grayColor.shape[::-1], None, None)
+    (ret, matrix, distortion, r_vecs, t_vecs) = cv2.calibrateCamera(threedpoints, twodpoints, gray_color.shape[::-1], None, None)
 
     # Displaying required output
     print("\n\nCamera matrix: \n")
@@ -143,7 +150,7 @@ def calibrate_camera():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    #generate_video_frames('assets/G3DCV2021_data/cam1 - static/calibration.mov', False)
+    # generate_video_frames('assets/G3DCV2021_data/cam1 - static/calibration.mov', False)
     calibrate_camera()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
